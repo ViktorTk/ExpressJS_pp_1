dcbee6b08e902bc118bcef18aeb9e9435a631c83
Viktor <viktor.avgn@yandex.ru>
2026-04-29 21:44:16 +0700

    получение данных через GET-запрос

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express')
 
 const app = express() // its working
 
-app.get('/', (req, res, next) => {
-  res.send('its working')
+const products = ['Apple', 'Pen', 'Computer']
+
+app.get(
+  '/',
+  (
+    req, // request
+    res, // response
+    next, // функция промежуточной обработки
+  ) => {
+    res.send('its working')
+  },
+)
+app.get('/products', (req, res, next) => {
+  res.send(products)
 })
 
+app.put
+app.post
+app.delete
+
+// запуск сервера; 1 аргумент - номер порта, 2 аргумент - callback-функция при запуске сервера;
 app.listen(5000, () => {
   console.log('Its started', new Date())
 })
+
+// закончил урок 2.3
